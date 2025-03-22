8b4dfd3d57a07de1f76272dded2e5f419e7ccb89
pratham-exe <prathamrao001@gmail.com>
2025-03-22 18:41:19 +0530

    Installation script added

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERNAME=$(whoami)
+
+# Stop digilinux if already running
+sudo systemctl stop digilinux 2>/dev/null || true
+
+echo "Creating /opt/digilinux directory"
+sudo mkdir -p /opt/digilinux
+sudo cp -r * /opt/digilinux/
+
+echo "Setting up systemd service"
+sed "s/{{USERNAME}}/$USERNAME/" digilinux.service | sudo tee /etc/systemd/system/digilinux.service >/dev/null
+
+# Reload systemd services
+sudo systemctl daemon-reload
+
+echo "Enabling digilinux service"
+sudo systemctl enable digilinux
+
+echo "Starting digilinux service"
+sudo systemctl start digilinux
+
+# Check digilinux service
+sudo systemctl status digilinux
+
+echo "Digital Wellbeing for Linux is installed and running. Visit http://127.0.0.1:5000 to check your time spent."
