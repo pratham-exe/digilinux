f14c13e96d80863dd22a995afaa2981e38f7f32e
pratham-exe <prathamrao001@gmail.com>
2025-03-24 09:15:31 +0530

    Reset at midnight

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 digital_wellbeing_hash_map = {}
 sleep_mode = False
+sleep_reset_hash_map = None
 lock = Lock()
 
 
@@ -24,6 +25,18 @@ def update_digital_wellbeing_data():
                 digital_wellbeing_hash_map = update_digital_wellbeing_hash_map(digital_wellbeing_hash_map, current_digital_wellbeing_list)
 
             time.sleep(1)
+
+
+def reset_hash_map_at_midnight():
+    global digital_wellbeing_hash_map
+
+    while True:
+        current_time = time.localtime()
+        if current_time.tm_hour == 0 and current_time.tm_min == 0:
+            with lock:
+                digital_wellbeing_hash_map.clear()
+
+        time.sleep(60)
 
 
 @app.route("/")
@@ -44,21 +57,25 @@ def get_data_from_main():
 
 @app.route("/sleep", methods=["POST"])
 def pause_time():
-    global sleep_mode
+    global sleep_mode, sleep_reset_hash_map
     if not sleep_mode:
         with lock:
             sleep_mode = True
+            sleep_reset_hash_map = time.localtime()
 
     return "OK", 200
 
 
 @app.route("/resume", methods=["POST"])
 def resume_time():
-    global sleep_mode
+    global sleep_mode, sleep_reset_hash_map
     if sleep_mode:
         with lock:
             resume_time_for_all_applications(digital_wellbeing_hash_map)
             sleep_mode = False
+            if sleep_reset_hash_map is not None and sleep_reset_hash_map.tm_mday != time.localtime().tm_mday:
+                digital_wellbeing_hash_map.clear()
+                sleep_reset_hash_map = None
 
     return "OK", 200
 
@@ -66,5 +83,8 @@ def resume_time():
 if __name__ == "__main__":
     background_thread = threading.Thread(target=update_digital_wellbeing_data, daemon=True)
     background_thread.start()
+
+    reset_hash_map_thread = threading.Thread(target=reset_hash_map_at_midnight, daemon=True)
+    reset_hash_map_thread.start()
 
     app.run()
